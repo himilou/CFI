c4a06b4a728cbfbc42a9859fc371e2528c5860c8
james hardy <himilou@hotmail.com>
2025-08-19 12:55:28 -0400

    privacy more info etc

---
+++ Pages/whyus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlyMeToAtp.Pages
+{
+    public class whyusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
